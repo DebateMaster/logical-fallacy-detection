2b7acb42a8e04df0475876013e6ec7eb694868ce
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-11-19 19:54:05 +0100

    Add simple ping-pong Server

---
+++ app.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+from flask import request, Flask, jsonify, Response, send_from_directory
+
+import dotenv
+
+dotenv.load_dotenv()
+app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+
+@app.route('/ping')
+def ping():
+    logger.info('ping received')
+    return 'pong'
+
+
+if __name__ == '__main__':
+    app.run()
