0d0bc4a5b4fe97b19085e044fd1b2c7449cadfea
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-11-30 14:30:31 +0100

    devops: setup docker deploy

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,18 @@ if [ "$(sudo docker ps -a -q -f name=$SERVER_CONTAINER)" ]; then
         sudo docker stop $SERVER_CONTAINER
     fi
 
+    if [ "$(sudo docker ps -aq -f status=restarting -f name=$SERVER_CONTAINER)" ]; then
+        # cleanup
+        sudo docker stop $SERVER_CONTAINER
+    fi
+
     # delete container
-    sudo docker rm $SERVER_CONTAINER
+    sudo docker rm -f $SERVER_CONTAINER
 fi
 
 # Configure the container runtime
-sudo nvidia-ctk runtime configure --runtime=docker
+# sudo nvidia-ctk runtime configure --runtime=docker
 # Run the server
-sudo docker run --gpus all -d -p 12023:12023 --name $SERVER_CONTAINER --restart unless-stopped $SERVER_CONTAINER nvidia-smi 
+sudo docker run --gpus all -d -p 12023:12023 --name $SERVER_CONTAINER nvidia/cuda:12.0.0-base-ubuntu20.04 nvidia-smi
 sudo docker ps
 sudo docker logs $SERVER_CONTAINER
