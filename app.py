a99108bc24b031d68f538e5374f1d4d73e75e8c8
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-12-02 00:14:27 +0100

    Add CORS header

--- app.py
+++ app.py
@@ -12,16 +12,21 @@ app = Flask(__name__)
 logger = logging.getLogger(__name__)
 
 
-@app.route('/text_predict')
-# @server.check_api_token_header # TODO
-def get_predict_handler() -> Response:
-    logger.info('Received GET request')
-    text = request.args.get('text')
-    prediction = network.predict(text)
-
-    response = make_response(jsonify(prediction=prediction))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+@app.route('/text_predict', methods=['GET', 'OPTIONS'])
+def get_predict_handler():
+    if request.method == 'OPTIONS':
+        # Preflight request handling
+        response = Flask.make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Max-Age'] = '3600'
+        return response
+    else:
+        logger.info('Received GET request')
+        text = request.args.get('text')
+        prediction = network.predict(text)
+        return jsonify(prediction=prediction)
 
 
 if __name__ == '__main__':
