8fa1ffbfed485226b586b37197e3eed1c12b3e17
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-12-02 00:25:45 +0100

    Add CORS header

--- app.py
+++ app.py
@@ -12,27 +12,25 @@ app = Flask(__name__)
 logger = logging.getLogger(__name__)
 
 
-@app.route('/text_predict', methods=['GET', 'OPTIONS'])
+@app.route('/text_predict', methods=['GET'])
 def get_predict_handler():
     if request.method == 'OPTIONS':
-        # Preflight request handling
         response = Flask.make_response()
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        response.headers['Access-Control-Max-Age'] = '3600'
-        return response
     else:
         logger.info('Received GET request')
         text = request.args.get('text')
         prediction = network.predict(text)
         response = make_response(jsonify(prediction=prediction))
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, User-Agent'
-        response.headers['Access-Control-Max-Age'] = '86400'
-        response.headers['Access-Control-Request-Method'] = 'GET'
-        return response
+    
+    # Preflight request handling
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, User-Agent'
+    response.headers['Access-Control-Max-Age'] = '86400'
+    response.headers['Access-Control-Request-Method'] = 'GET'
+    
+    return response
+        
 
 if __name__ == '__main__':
     app.run()
